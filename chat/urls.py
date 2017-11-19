c087a87236077349f646787692525710ef75fc06
Kevin Ndung'u <kevgathuku@gmail.com>
2017-11-19 23:46:06 +0300

    Add route to generate token

--- chat/urls.py
+++ chat/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'$', views.chat, name="chat"),
     url(r'^$', views.chat, name="chat"),
+    url(r'token$', views.token, name="token"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,34 @@
+from django.conf import settings
+from django.http import JsonResponse
 from django.shortcuts import render
+
+from faker import Faker
+from twilio.jwt.access_token import AccessToken
+from twilio.jwt.access_token.grants import ChatGrant
+
+fake = Faker()
 
 
 def chat(request):
     return render(request, 'chat/index.html')
+
+
+def token(request):
+    identity = request.GET.get('identity', fake.user_name()).encode('utf-8')
+    account_sid = settings.TWILIO_ACCOUNT_SID
+    api_key = settings.TWILIO_API_KEY
+    api_secret = settings.TWILIO_API_SECRET
+    chat_service_sid = settings.TWILIO_CHAT_SERVICE_SID
+
+    token = AccessToken(account_sid, api_key, api_secret, identity=identity)
+
+    if chat_service_sid:
+        chat_grant = ChatGrant(service_sid=chat_service_sid)
+        token.add_grant(chat_grant)
+
+    response = {
+        'identity': identity,
+        'token': token.to_jwt().decode('utf-8')
+    }
+
+    return JsonResponse(response, safe=False)
