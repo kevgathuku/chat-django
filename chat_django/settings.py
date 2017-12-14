28ff2a5bb4f23ac8bcd9a90bec3b672c1a6669aa
Kevin Ndung'u <kevgathuku@gmail.com>
2017-12-14 17:05:44 +0300

    Add missing settings for whitenoise

--- chat_django/settings.py
+++ chat_django/settings.py
@@ -42,12 +42,14 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'chat.apps.ChatConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
