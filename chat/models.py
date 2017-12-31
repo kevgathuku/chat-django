c356bcab6334a37e193c9447925915c16ff1f19e
Kevin Ndung'u <kevgathuku@gmail.com>
2017-12-31 11:37:27 +0300

    Create Room model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    slug = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
