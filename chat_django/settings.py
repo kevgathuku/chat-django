80d2b898ff394a1efc480f5865a24ba53f566445
Kevin Ndung'u <kevgathuku@gmail.com>
2017-12-31 11:44:28 +0300

    Install django-extensions

--- chat_django/settings.py
+++ chat_django/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+    'django_extensions',
     'chat.apps.ChatConfig',
 ]
 
