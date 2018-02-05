e84de60bd8dc07a2c03e672bb0f911280a24298d
Kevin Ndung'u <kevgathuku@gmail.com>
2018-02-05 14:49:29 +0300

    Add backend tests

--- chat/tests.py
+++ chat/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Room
+
+
+class HomePageTest(TestCase):
+    def test_all_rooms_are_displayed_in_homepage(self):
+        room_1 = Room.objects.create(
+            name='room 1',
+            slug='room-1',
+            description='This is the 1st room'
+        )
+        room_2 = Room.objects.create(
+            name='room 2',
+            slug='room-2',
+            description='This is the 2nd room'
+        )
+
+        response = self.client.get('/')
+
+        self.assertContains(response, room_1.name)
+        self.assertContains(response, room_1.slug)
+
+        self.assertContains(response, room_2.name)
+        self.assertContains(response, room_2.slug)
+
+
+class RoomDetailTest(TestCase):
+
+    def test_room_details_are_present_in_room_page(self):
+        room_1 = Room.objects.create(
+            name='room 1',
+            slug='room-1',
+            description='This is the 1st room'
+        )
+
+        response = self.client.get('/rooms/{}/'.format(room_1.slug))
+
+        self.assertContains(response, room_1.name)
+        self.assertContains(response, room_1.description)
